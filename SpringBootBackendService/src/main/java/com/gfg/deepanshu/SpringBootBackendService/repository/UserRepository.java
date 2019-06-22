eabd71f1c865a9f71c28db0b78de07e1273d3f40
deepanshuganotra <ganotra.deep@gmail.com>
2019-06-22 10:52:07 +0530

    commmit again

--- SpringBootBackendService/src/main/java/com/gfg/deepanshu/SpringBootBackendService/repository/UserRepository.java
+++ SpringBootBackendService/src/main/java/com/gfg/deepanshu/SpringBootBackendService/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.gfg.deepanshu.SpringBootBackendService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
